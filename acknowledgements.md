547e867c094b208e82833e4328236980a5051d10
TanayB11 <tanaybiradar24@gmail.com>
2019-12-22 15:38:45 -0800

    Initial commit - exported keras model, weights

---
+++ acknowledgements.md
@@ -0,0 +1,7 @@
+# Acknowledgements
+
+I'd like to thank the following people/pages for providing resources that have especially helped me.
+
+[integeruser on Github](https://github.com/integeruser/CASIA-HWDB1.1-cnn)
+
+[想飞的石头在知乎](https://zhuanlan.zhihu.com/p/24698483)
---
+++ src/train.py
@@ -0,0 +1,57 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# Most of this code comes from https://github.com/integeruser/CASIA-HWDB1.1-cnn
+import json
+import sys
+import time
+
+import numpy as np
+np.random.seed(1337)
+
+import h5py
+from keras.layers.convolutional import Conv2D, MaxPooling2D
+from keras.layers.core import Dense, Dropout, Flatten
+from keras.models import Sequential
+from keras.regularizers import l2
+
+subset_filepath = "../data/HWDB1.1subset.hdf5"
+
+# as described in http://yuhao.im/files/Zhang_CNNChar.pdf
+model = Sequential()
+model.add(Conv2D(64, (3, 3), weights=[np.random.normal(0, 0.01, size=(3, 3, 1, 64)), np.zeros(64)],
+                        activation='relu', padding='same', strides=(1, 1),
+                        input_shape=(1, 64, 64), data_format='channels_first'))
+model.add(MaxPooling2D(pool_size=(2, 2), strides=(2, 2)))
+
+model.add(Conv2D(128, (3, 3), weights=[np.random.normal(0, 0.01, size=(3, 3, 64, 128)), np.zeros(128)],
+                        activation='relu', padding='same', strides=(1, 1)))
+model.add(MaxPooling2D(pool_size=(2, 2), strides=(2, 2)))
+
+model.add(Conv2D(256, (3, 3), weights=[np.random.normal(0, 0.01, size=(3, 3, 128, 256)), np.zeros(256)],
+                        activation='relu', padding='same', strides=(1, 1)))
+model.add(MaxPooling2D(pool_size=(2, 2), strides=(2, 2)))
+
+model.add(Flatten())
+
+model.add(Dropout(0.5))
+model.add(Dense(1024, activation='relu'))
+
+model.add(Dropout(0.5))
+model.add(Dense(200, activation='softmax'))
+
+model.compile(loss='categorical_crossentropy', optimizer='adadelta', metrics=['accuracy'])
+
+timestamp = int(time.time())
+with open('../output/model.json' % timestamp, 'w') as f:
+    d = json.loads(model.to_json())
+    json.dump(d, f, indent=4)
+
+with h5py.File(subset_filepath, 'r') as f:
+    model.fit(f['trn/x'], f['trn/y'], validation_data=(f['vld/x'], f['vld/y']),
+              epochs=15, batch_size=128, shuffle='batch', verbose=1)
+
+    score = model.evaluate(f['tst/x'], f['tst/y'], verbose=0)
+    print('Test score:', score[0])
+    print('Test accuracy:', score[1])
+
+    model.save_weights('../output/weights.hdf5' % (timestamp, score[1]))
