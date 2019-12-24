71879f40067fb9ed435fec93112f7ee9e793cd9a
TanayB11 <tanaybiradar24@gmail.com>
2019-12-24 14:26:02 -0800

    Creating a new CNN model - preprocessing dataset

--- src/preprocess.py
+++ src/preprocess.py
@@ -1,12 +1,41 @@
-import imageio
+import os
 import numpy as np
-import h5py
+import tensorflow as tf
+import cv2
+from matplotlib import pyplot
+# import pickle
 
-f = h5py.File('../output/in1.hdf5', 'r')
-dset = f['key']
-data = np.array(dset[:,:,:])
-file = 'test.png' # or .jpg
-imageio.imwrite(file, data)
+train_path = '../data/temptrn' # TODO: CHANGE LATER TO ACTUAL TRAINING SET
+test_path = '../data/temptst'
 
-# TODO: Make sure in1.hdf5 works so we can input it into the model to predict
-# Also recover the code that converted png to hdf5
+def extract_data(path):
+    labels = []
+    data = []
+    for i, label in enumerate(sorted(os.listdir(path))):
+        if (label == '.DS_Store'): # Gets rid of the problem of that file
+            pass
+        else:
+            for j, filename in enumerate(os.listdir(os.path.join(path, label))):
+                image = cv2.imread(os.path.join(path, label, filename))
+                image = cv2.resize(image, (64, 64))
+                image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+                data.append(image)
+                labels.append(i-1) # Will have to look up in dictionary later for final result labels = pickle.load(open('char_dict', 'rb'))
+    # cv2.imshow('abc', data[0]); cv2.waitKey(0); cv2.destroyAllWindows() # Show the image at data[0]
+    return (np.array(data), np.array(labels))
+
+def plot_images():
+    # plot first few images
+    for i in range(9): # Train images
+        pyplot.subplot(330 + 1 + i)
+        pyplot.imshow(trainX[i], cmap=pyplot.get_cmap('gray'))
+    pyplot.show()
+    for i in range(9): # Test images
+        pyplot.subplot(330 + 1 + i)
+        pyplot.imshow(testX[i], cmap=pyplot.get_cmap('gray'))
+    pyplot.show()
+
+(trainX, trainY) = extract_data(train_path)
+(testX, testY) = extract_data(test_path)
+print('Train: X=%s, y=%s' % (trainX.shape, trainY.shape))
+print('Test: X=%s, y=%s' % (testX.shape, testY.shape))
--- src/train.py
+++ src/train.py
@@ -1,51 +1,40 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
 # Most of this code comes from https://github.com/integeruser/CASIA-HWDB1.1-cnn
-import json
-import sys
-import time
-
+import json, sys, time
 import numpy as np
-np.random.seed(1337)
-
-import h5py
+np.random.seed(3141)
 from keras.layers.convolutional import Conv2D, MaxPooling2D
 from keras.layers.core import Dense, Dropout, Flatten
 from keras.models import Sequential
-from keras.regularizers import l2
 
-subset_filepath = "../data/HWDB1.1subset.hdf5"
-
+# This is the CNN model
+# https://github.com/integeruser/CASIA-HWDB1.1-cnn/blob/master/src/3-train_subset.py
 # as described in http://yuhao.im/files/Zhang_CNNChar.pdf
 model = Sequential()
 model.add(Conv2D(64, (3, 3), weights=[np.random.normal(0, 0.01, size=(3, 3, 1, 64)), np.zeros(64)],
-                        activation='relu', padding='same', strides=(1, 1),
-                        input_shape=(1, 64, 64), data_format='channels_first'))
+    activation='relu', padding='same', strides=(1, 1),
+    input_shape=(1, 64, 64), data_format='channels_first'))
 model.add(MaxPooling2D(pool_size=(2, 2), strides=(2, 2)))
-
 model.add(Conv2D(128, (3, 3), weights=[np.random.normal(0, 0.01, size=(3, 3, 64, 128)), np.zeros(128)],
-                        activation='relu', padding='same', strides=(1, 1)))
+    activation='relu', padding='same', strides=(1, 1)))
 model.add(MaxPooling2D(pool_size=(2, 2), strides=(2, 2)))
-
 model.add(Conv2D(256, (3, 3), weights=[np.random.normal(0, 0.01, size=(3, 3, 128, 256)), np.zeros(256)],
-                        activation='relu', padding='same', strides=(1, 1)))
+    activation='relu', padding='same', strides=(1, 1)))
 model.add(MaxPooling2D(pool_size=(2, 2), strides=(2, 2)))
-
 model.add(Flatten())
-
 model.add(Dropout(0.5))
 model.add(Dense(1024, activation='relu'))
-
 model.add(Dropout(0.5))
 model.add(Dense(200, activation='softmax'))
+myAdam = keras.optimizers.Adam(learning_rate=0.001, beta_1=0.9, beta_2=0.999, amsgrad=False)
+model.compile(loss='categorical_crossentropy', optimizer=myAdam, learning_rate=0.05 metrics=['accuracy'])
 
-model.compile(loss='categorical_crossentropy', optimizer='adadelta', metrics=['accuracy'])
-
+# Save the model to a JSON file for easy import
 timestamp = int(time.time())
 with open('../output/model.json' % timestamp, 'w') as f:
     d = json.loads(model.to_json())
     json.dump(d, f, indent=4)
 
+# Training - TODO: Change filetype input
 with h5py.File(subset_filepath, 'r') as f:
     model.fit(f['trn/x'], f['trn/y'], validation_data=(f['vld/x'], f['vld/y']),
               epochs=15, batch_size=128, shuffle='batch', verbose=1)
