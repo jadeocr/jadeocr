eebacd624acd8361f1dc3ba444fabdba179fe674
TanayB11 <tanaybiradar24@gmail.com>
2020-04-11 22:00:40 -0700

    Add navbar skeleton

---
+++ src/components/Navigation.vue
@@ -0,0 +1,25 @@
+<template>
+  <div id="#navigation">
+    <a href="#" id="nav-title">chinese-ocr</a>
+  </div>
+</template>
+
+<script>
+export default {
+  
+}
+</script>
+
+<style scoped>
+#navigation{
+  line-height: 57px;
+  font-size: 42px;
+}
+#nav-title{
+  position: absolute;
+  width: 250px;
+  height: 57px;
+  left: 25px;
+  top: 50px;
+}
+</style>
