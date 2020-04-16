85c1366bc3e0de57d56a9474782314932d6e3751
TanayB11 <tanaybiradar24@gmail.com>
2020-04-15 18:32:49 -0700

    Update ESLint config

--- chinese-ocr-webapp/src/App.vue
+++ chinese-ocr-webapp/src/App.vue
@@ -1,37 +1,37 @@
 <template>
   <div id="app">
-    <Navigation/>
-    <Title/>
+    <Navigation />
+    <Title />
   </div>
 </template>
 
 <script>
-import Navigation from './components/Navigation.vue'
-import Title from "./components/Title.vue"
+import Navigation from "./components/Navigation.vue";
+import Title from "./components/Title.vue";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
     Navigation,
     Title
   }
-}
+};
 </script>
 
 <style>
-@import url('https://fonts.googleapis.com/css2?family=Open+Sans:wght@300&display=swap');
-@import './assets/css/output.css';
+@import url("https://fonts.googleapis.com/css2?family=Open+Sans:wght@300&display=swap");
+@import "./assets/css/output.css";
 
 /* Styling the background */
-body{
+body {
   background-size: cover;
   background: #121212;
 }
 
-#app{
+#app {
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  font-family: 'Open Sans', sans-serif;
+  font-family: "Open Sans", sans-serif;
   font-weight: 300;
   overflow: scroll;
   color: #ffffff;
