0f8672a1c853ee63ac48dcf803a76b5d55a6b3f3
TanayB11 <tanaybiradar24@gmail.com>
2020-04-16 09:58:00 -0700

    Add 404 page

--- chinese-ocr-webapp/src/router/index.js
+++ chinese-ocr-webapp/src/router/index.js
@@ -5,6 +5,8 @@ import Landing from '../views/Landing.vue'
 
 Vue.use(VueRouter)
 
+const mode = 'history' // Removes # in URL: Configure history mode for production
+
 const routes = [
 	{
 		path: '/',
@@ -18,10 +20,15 @@ const routes = [
 		// this generates a separate chunk (about.[hash].js) for this route
 		// which is lazy-loaded when the route is visited.
 		component: () => import('../views/Login.vue')
+	},
+	{
+		path: '*',
+		component: () => import ('../views/NotFound.vue')
 	}
 ]
 
 const router = new VueRouter({
+	mode,
 	routes
 })
 
---
+++ chinese-ocr-webapp/src/views/NotFound.vue
@@ -0,0 +1,25 @@
+<template>
+	<div class='container mx-auto text-base text-2xl sm:text-3xl md:text-4xl lg:text-5xl xl:text-6xl
+	mt-24 sm:mt-32 md:mt-40 lg:mt-56 xl:mt-64'>
+    <p>囧</p>
+    <p>404 Not Found</p>
+    <router-link :to='{ name: "landing" }'
+    class='btn bg-purple-500 hover:bg-purple-600 mt-8 px-3 py-1 
+    text-base sm:text-xl md:text-2xl lg:text-3xl xl:text-4xl rounded-lg'
+    >
+      Home | 返回
+    </router-link>
+	</div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style scoped>
+.container{
+	text-align: center;
+}
+</style>
