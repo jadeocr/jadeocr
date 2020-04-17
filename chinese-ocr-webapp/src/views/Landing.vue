04d29c92449ebcab4c6e9fc66385847758e3dcbb
TanayB11 <tanaybiradar24@gmail.com>
2020-04-17 16:08:00 -0700

    Add sign in with Google

--- chinese-ocr-webapp/src/views/Landing.vue
+++ chinese-ocr-webapp/src/views/Landing.vue
@@ -1,49 +1,16 @@
 <template>
 	<div>
 			<Title/>
-			<!-- Displays database properties -->
-			<h1>{{ testDB }}</h1> 
 	</div>
 </template>
 
 <script>
 import Title from '../components/Title.vue'
-import { db } from '../firebase/db.js'
 
 export default {
 	name: 'Landing',
 	components: {
 		Title
-	},
-	data(){
-		return{
-			testDB: ''
-		}
-	},
-	mounted: function(){
-		if (location.hostname === "localhost") { // Checks for local emulator
-			db.settings({
-				host: "localhost:3000",
-				ssl: false
-			})
-		}
-
-		db.collection('test-collection').doc('test-items').get()
-		.then(doc => {
-			if (!doc.exists) {
-				console.log('No such document!');
-			} else {
-				this.testDB = ('Document data:', doc.data());
-			}
-		})
-		.catch(err => {
-			console.log('Error getting document', err);
-		})
-
-		db.collection('test-collection').doc('test-items').set({
-			name: 'foo',
-			age: 'bar'
-		})
 	}
 }
 </script>
--- chinese-ocr-webapp/src/views/Login.vue
+++ chinese-ocr-webapp/src/views/Login.vue
@@ -1,15 +1,40 @@
 <template>
   <div>
-    
+    <button @click='signIn'
+      class='btn bg-purple-500 hover:bg-purple-600 mt-8 px-3 py-1 
+      text-base sm:text-xl md:text-2xl lg:text-3xl xl:text-4xl rounded-lg'
+      >
+        Log In
+      </button>
   </div>
 </template>
 
 <script>
-export default {
+import * as firebase from 'firebase'
+import credentials from '../firebase/credentials'
+firebase.initializeApp(credentials.firebaseConfig)
 
+export default {
+  name: 'Login',
+  data() {
+    return {
+    }
+  },
+  methods: {
+    signIn: function() {
+      var provider = new firebase.auth.GoogleAuthProvider()
+      firebase.auth().signInWithRedirect(provider)
+    },
+    signOut: function() {
+      firebase.auth().signOut()
+    },
+    getUsername: function() {
+      this.username = firebase.auth().currentUser.displayName
+    }
+  }
 }
 </script>
 
 <style>
-
+            
 </style>
