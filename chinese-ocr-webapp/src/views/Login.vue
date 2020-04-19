3fa8522152e62eb74da38fdf6af82e96903009cb
TanayB11 <tanaybiradar24@gmail.com>
2020-04-19 11:59:25 -0700

    Add form validation

--- chinese-ocr-webapp/src/views/Login.vue
+++ chinese-ocr-webapp/src/views/Login.vue
@@ -1,13 +1,13 @@
 <template>
 	<div>
 		<div class='m-auto w-full max-w-xs mt-8 md:mt-10 lg:mt-12 xl:mt-26'>
-			<form class='login-form bg-black shadow-md rounded px-8 pt-6 pb-8 mb-4 rounded-md'>
+			<form class='login-form bg-black shadow-md rounded px-8 pt-6 pb-8 mb-4 rounded-md' @submit='checkForm'>
 				<div class='mb-4'>
 					<label class='block opacity-86 text-sm font-bold mb-2'>
 						Email
 					</label>
 					<input v-model='email' class='bg-gray-300 shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 
-					leading-tight focus:outline-none focus:shadow-outline' type='email' placeholder='alice@example.com'>
+					leading-tight focus:outline-none focus:shadow-outline' type='text' placeholder='alice@example.com'>
 				</div>
 				<div class='mb-6'>
 					<label class='block opacity-86 text-sm font-bold mb-2'>
@@ -15,14 +15,15 @@
 					</label>
 					<input v-model='password' class='bg-gray-300 shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 mb-3 
 					leading-tight focus:outline-none focus:shadow-outline' type='password' placeholder='********'>
+					<p v-if='errors.length' class="text-red-500 text-sm mt-1 -mb-1">{{ errors }}</p>
 				</div>
 				<div class='flex items-center justify-between'>
 					<button @click='signInWithEmail'
-					class='btn-purple opacity-86 text-white font-bold py-2 px-4 rounded' type='button'>
+					class='btn-purple opacity-86 text-white font-bold py-2 px-4 rounded' type='submit'>
 						Sign In
 					</button>
 					<button @click='signUpWithEmail'
-					class='btn-purple opacity-86 text-white font-bold py-2 px-4 rounded' type='button'>
+					class='btn-purple opacity-86 text-white font-bold py-2 px-4 rounded' type='submit'>
 						Sign Up
 					</button>
 				</div>
@@ -48,16 +49,38 @@ export default {
 	data() {
 		return {
 			email: '',
-			password: ''
+			password: '',
+			errors: []
 		}
 	},
 	methods: {
+		checkForm: function(e) {
+			this.errors = []
+
+			if ((!this.email) || (!this.password)){
+				this.errors.push('Please complete all fields ')
+			} else if (!this.validEmail(this.email)) {
+				this.errors.push('Invalid Email')
+			} else {
+				this.error = []
+			}
+
+			this.errors = this.errors.toString()
+			this.email = ''
+			this.password = ''
+			e.preventDefault()
+		},
+		validEmail: function (email) {
+			const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+			return re.test(email)
+		},
 		signUpWithEmail: function() {
 			firebase.auth().createUserWithEmailAndPassword(this.email.trim(), this.password.trim())
 			.catch(function(error) {
 				console.log(error.code)
 				console.log(error.message)
 			})
+			this.error = []
 		},
 		signInWithEmail: function() {
 			firebase.auth().signInWithEmailAndPassword(this.email.trim(), this.password.trim())
@@ -65,10 +88,11 @@ export default {
 				console.log(error.code)
 				console.log(error.message)
 			})
+			this.error = []
 		},
 		signInWithGoogle: function() {
 			var provider = new firebase.auth.GoogleAuthProvider()
-      firebase.auth().signInWithRedirect(provider)
+			firebase.auth().signInWithRedirect(provider)
 		}
 	}
 }
