861eb5c35a1b47940929587e2cc779f6145f6bfe
TanayB11 <tanaybiradar24@gmail.com>
2020-04-22 08:46:54 -0700

    Recompartmentalize landing page

---
+++ chinese-ocr-webapp/src/components/GetStartedButton.vue
@@ -0,0 +1,35 @@
+<template>
+	<div>
+		<router-link :to='{ name: "login" }'
+		class='btn mt-8 px-3 py-1 text-base sm:text-xl md:text-2xl lg:text-3xl xl:text-4xl rounded-lg'>
+			Get Started | 开始
+		</router-link>
+	</div>
+</template>
+
+<script>
+export default {
+	name: 'GetStartedButton'
+}
+</script>
+
+<style scoped>
+.btn{
+  animation: 3s btnopacity;
+  background-color: #bb86fc;
+  opacity: 0.87;
+}
+
+.btn:hover {
+	opacity: 0.75;
+}
+
+@keyframes btnopacity{
+  from{
+    opacity: 0;
+  }
+  to{
+    opacity: 0.87;
+  }
+}
+</style>
--- chinese-ocr-webapp/src/views/Landing.vue
+++ chinese-ocr-webapp/src/views/Landing.vue
@@ -1,20 +1,81 @@
 <template>
 	<div>
-			<Title/>
+		<div class='container max-w-none mt-1 sm:mt-2 md:mt-3 lg:mt-5 xl:mt-6'>
+			<div class='col-1 mr-0 sm:-mr-2 md:-mr-3 lg:-mr-4 xl:-mr-5'>
+				<img src='../assets/title-image.png' alt='An image of the Chinese character 文, meaning "written language"'>
+			</div>
+			<div class='col-2 text-base text-2xl sm:text-3xl md:text-4xl lg:text-5xl xl:text-6xl'>
+				<p>Learn Chinese</p>
+				<p class='mb-4'>without distractions.</p>
+				<GetStartedButton/>
+			</div>
+		</div>
 	</div>
 </template>
 
 <script>
-import Title from '../components/Title.vue'
+import GetStartedButton from '../components/GetStartedButton'
 
 export default {
 	name: 'Landing',
 	components: {
-		Title
+		GetStartedButton
 	}
 }
 </script>
 
-<style>
+<style scoped>
+img {
+  max-width: 93.547%;
+}
 
+.container{
+  display: flex;
+  flex-direction: column;
+}
+
+.col-1{
+  flex: 1;
+  animation: 1.25s shiftopacity;
+  opacity: 0.87;
+}
+
+@keyframes shiftopacity {
+  from{
+    opacity: 0;
+  }
+  to{
+    opacity: 0.87;
+  }
+}
+
+.col-2{
+  text-align: center;
+  flex: 1;
+  animation: 1.25s slidedown;
+  margin-top: -6vh;
+  opacity: .87;
+}
+
+@keyframes slidedown{
+  from{
+    margin-top: -20vh;
+    opacity: 0;
+  }
+  to{
+    margin-top: -8vh;
+    opacity: .87;
+  }
+}
+
+@media (min-width: 600px){
+  .container{
+    flex-direction: row;
+    align-items: center;
+  }
+  .col-2{
+    text-align: left;
+    margin-top: -8vh;
+  }
+}
 </style>
