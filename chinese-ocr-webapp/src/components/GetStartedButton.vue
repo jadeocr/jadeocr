c9462021eab3e45e3793e74c247a5297784753a8
TanayB11 <tanaybiradar24@gmail.com>
2020-04-22 08:52:15 -0700

    Apply Chinese font class

--- chinese-ocr-webapp/src/components/GetStartedButton.vue
+++ chinese-ocr-webapp/src/components/GetStartedButton.vue
@@ -1,7 +1,7 @@
 <template>
 	<div>
 		<router-link :to='{ name: "login" }'
-		class='btn mt-8 px-3 py-1 text-base sm:text-xl md:text-2xl lg:text-3xl xl:text-4xl rounded-lg'>
+		class='chinese btn mt-8 px-3 py-1 text-base sm:text-xl md:text-2xl lg:text-3xl xl:text-4xl rounded-lg'>
 			Get Started | 开始
 		</router-link>
 	</div>
