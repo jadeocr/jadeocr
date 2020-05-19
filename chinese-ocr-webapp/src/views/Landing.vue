406bfe6503f0a5b4df28f7bde24c1e6d2104dd34
TanayB11 <tanaybiradar24@gmail.com>
2020-05-19 12:50:38 -0700

    Clean up landing code

--- chinese-ocr-webapp/src/views/Landing.vue
+++ chinese-ocr-webapp/src/views/Landing.vue
@@ -1,15 +1,20 @@
 <template>
-	<div>
-		<div class='container max-w-none mt-1 sm:mt-2 md:mt-3 lg:mt-5 xl:mt-6 font-light'>
-			<div class='col-1 opacity-87 mr-0 sm:-mr-2 md:-mr-3 lg:-mr-4 xl:-mr-5'>
-				<img src='../assets/title-image.png' alt='An image of the Chinese character 文, meaning "written language"'>
+	<div id='landing'>
+		<div class='slide container opacity-87 font-light grid grid-cols-1 md:grid-cols-2 m-auto'>
+			<div class='col-span-1'>
+        <img src='../assets/title-image.png' alt='An image of the Chinese character 文, meaning "written language"'>
 			</div>
-			<div class='opacity-87 col-2 text-base text-2xl sm:text-3xl md:text-4xl lg:text-5xl xl:text-6xl'>
-				<p>Learn Chinese</p>
-				<p class='mb-4'>without distractions.</p>
+			<div class='opacity-87 col-span-1 text-center md:text-left my-auto md:ml-8'>
+        <div class='mb-6 text-2xl md:text-3xl lg:text-4xl xl:text-5xl'>
+          <p>Learn Chinese</p>
+          <p class=''>without distractions.</p>
+        </div>
 				<GetStartedButton/>
 			</div>
 		</div>
+    <div>
+      <!-- V arrow glowing -->
+    </div>
 	</div>
 </template>
 
@@ -25,55 +30,18 @@ export default {
 </script>
 
 <style scoped>
-img {
-  max-width: 93.547%;
-}
-
-.container{
-  display: flex;
-  flex-direction: column;
-}
-
-.col-1{
-  flex: 1;
-  animation: 1.25s shiftopacity;
-}
-
-@keyframes shiftopacity {
-  from{
-    opacity: 0;
-  }
-  to{
-    opacity: 0.87;
-  }
-}
-
-.col-2{
-  text-align: center;
-  flex: 1;
-  animation: 1.25s slidedown;
-  margin-top: -6vh;
+.slide {
+  animation: .75s slidedown;
 }
 
 @keyframes slidedown{
   from{
-    margin-top: -20vh;
+    margin-top: -5vh;
     opacity: 0;
   }
   to{
-    margin-top: -8vh;
+    margin-top: 0vh;
     opacity: .87;
-  }
-}
-
-@media (min-width: 600px){
-  .container{
-    flex-direction: row;
-    align-items: center;
-  }
-  .col-2{
-    text-align: left;
-    margin-top: -8vh;
   }
 }
 </style>
