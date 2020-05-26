d305e55629dbc1019b5c3b32c416210a93b4d2b4
TanayB11 <tanaybiradar24@gmail.com>
2020-05-26 12:02:35 -0700

    Add navigation guard

--- chinese-ocr-webapp/src/router/index.js
+++ chinese-ocr-webapp/src/router/index.js
@@ -1,5 +1,6 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
+import store from '../store/index'
 
 import Landing from '../views/Landing'
 
@@ -20,42 +21,66 @@ const routes = [
 	},
 	{
 		path: '/dashboard',
-		redirect: '/dashboard/learn'
+		redirect: '/dashboard/learn',
+		meta: {
+			requiresAuth: true
+		}
 	},
 	{
 		path: '/dashboard/learn',
 		name: 'learn',
-		component: () => import('../views/Learn')
+		component: () => import('../views/Learn'),
+		meta: {
+			requiresAuth: true
+		}
 	},
 	{
 		path: '/dashboard/decks',
 		name: 'decks',
-		component: () => import('../views/Decks')
+		component: () => import('../views/Decks'),
+		meta: {
+			requiresAuth: true
+		}
 	},
 	{
 		path: '/dashboard/stats',
 		name: 'stats',
-		component: () => import('../views/Stats')
+		component: () => import('../views/Stats'),
+		meta: {
+			requiresAuth: true
+		}
 	},
 	{
 		path: '/dashboard/profile',
 		name: 'profile',
-		component: () => import('../views/Profile')
+		component: () => import('../views/Profile'),
+		meta: {
+			requiresAuth: true
+		}
 	},
 	{
 		path: '/create',
 		name: 'create',
-		component: () => import('../views/Create')
+		component: () => import('../views/Create'),
+		meta: {
+			requiresAuth: true
+		}
 	},
 	{
 		path: '/review/:name',
 		props: true,
-		component: () => import('../views/Learn') // Make this point to the view for actually reviewing the cards
+		component: () => import('../views/Learn'), // Make this point to the view for actually reviewing the cards
+		meta: {
+			requiresAuth: true			
+		}
 	},
 	{
 		path: '/edit/:name',
 		props: true,
-		component: () => import('../views/Edit')
+		component: () => import('../views/Edit'),
+		meta: {
+			requiresAuth: true
+		}
 	},
 	{
 		path: '*',
@@ -66,6 +91,15 @@ const routes = [
 const router = new VueRouter({
 	mode,
 	routes
+})
+
+router.beforeEach((to, from, next) => {
+	const requiresAuth = to.matched.some(record => record.meta.requiresAuth)
+	if(requiresAuth && !store.state.signedIn) {
+		next('login')
+	} else {
+		next()
+	}
 })
 
 export default router
