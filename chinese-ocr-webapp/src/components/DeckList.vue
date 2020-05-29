3d484a32cfa9015df96c87b22f8850200e00ec9f
TanayB11 <tanaybiradar24@gmail.com>
2020-05-29 15:14:07 -0700

    Add hover to deck icon

--- chinese-ocr-webapp/src/components/DeckList.vue
+++ chinese-ocr-webapp/src/components/DeckList.vue
@@ -11,7 +11,7 @@
 								Learn
 							</div>
 						</router-link>
-						<div v-if='view == "decks"' class='mt-3 relative' id='edit-icon'>
+						<div v-if='view == "decks"' class='btn mt-3 relative' id='edit-icon'>
 							<router-link :to='{ path: `/edit/${$store.state.decks[i].name}` }'>
 								<svg class="bi bi-pencil-square" width="1em" height="1em" viewBox="0 0 16 16" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
 									<path d="M15.502 1.94a.5.5 0 0 1 0 .706L14.459 3.69l-2-2L13.502.646a.5.5 0 0 1 .707 0l1.293 1.293zm-1.75 2.456l-2-2L4.939 9.21a.5.5 0 0 0-.121.196l-.805 2.414a.25.25 0 0 0 .316.316l2.414-.805a.5.5 0 0 0 .196-.12l6.813-6.814z"/>
