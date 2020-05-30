f5c71756d7efeecd238db840bce3732516584f78
TanayB11 <tanaybiradar24@gmail.com>
2020-05-30 14:52:25 -0700

    Update review sheet layout

--- chinese-ocr-webapp/src/views/Review.vue
+++ chinese-ocr-webapp/src/views/Review.vue
@@ -1,24 +1,23 @@
 <template>
 	<div id='review'>
-		<div class="w-1/2 m-auto text-center lg:text-left lg:ml-16
-		mt-12 md:mt-18 xl:mt-24 opacity-87 text-xl lg:text-2xl xl:text-3xl font-normal">
+		<div class="w-1/2 m-auto text-center lg:text-left mt-12 md:mt-16 
+		opacity-87 text-xl lg:text-2xl xl:text-3xl font-normal">
 			{{ name }}
 		</div>
-		<div class='w-3/5 lg:w-1/2 m-auto mt-12 lg:mt-24 text-center'>	
-			<div class='bg-black rounded-md px-12 py-12 md:py-24 card'>
+		<div class='w-3/5 lg:w-1/2 m-auto mt-8 text-center'>	
+			<div class='bg-black rounded-md px-12 py-24 md:py-32 card'>
 				<!-- {{ currentWord }} --> HELLO
 			</div>
-			<div class="mt-8">
-				<div class='text-md md:text-lg lg:text-xl xl:text-2xl rounded-md
-				w-1/2 md:w-1/5 text-center m-auto btn btn-blue rounded-md px-4 py-2'>
-					Submit
-				</div>
-				<div class="mt-8 w-3/4 md:w-3/5 m-auto flex items-center justify-between opacity-87">
+			<div class="mt-10">
+				<div class="md:w-2/3 mt-4 m-auto flex items-center justify-between opacity-87">
 					<div class='btn btn-red rounded-md px-4 py-2'>
 						<svg class="bi bi-x" width="1.25em" height="1.25em" viewBox="0 0 16 16" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
 							<path fill-rule="evenodd" d="M11.854 4.146a.5.5 0 0 1 0 .708l-7 7a.5.5 0 0 1-.708-.708l7-7a.5.5 0 0 1 .708 0z"/>
 							<path fill-rule="evenodd" d="M4.146 4.146a.5.5 0 0 0 0 .708l7 7a.5.5 0 0 0 .708-.708l-7-7a.5.5 0 0 0-.708 0z"/>
 						</svg>
+					</div>
+					<div class="btn btn-blue mx-auto rounded-md py-2 px-4 text-sm">
+						Submit
 					</div>
 					<div class='btn btn-teal rounded-md px-4 py-2'>
 						<svg class="bi bi-check2" width="1.25em" height="1.25em" viewBox="0 0 16 16" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
