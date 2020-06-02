0e5b46e9b9f6b7cb44a76997a8a74686cf8f82b1
TanayB11 <tanaybiradar24@gmail.com>
2020-06-02 14:11:54 -0700

    Add bar chart for num of cards

--- chinese-ocr-webapp/src/views/Stats.vue
+++ chinese-ocr-webapp/src/views/Stats.vue
@@ -3,22 +3,71 @@
 		<div class='col-span-1'>
 			<Sidebar/>
 		</div>
-		<div class='col-span-1 ml-8 md:ml-0 mt-10 md:mt-20 p-8 md:px-8 mt-12 md:mt-0' id='page-content'>
+		<div class='col-span-1 ml-8 md:ml-0 mt-10 md:mt-20 p-8 md:px-8 mt-12 md:mt-0 overflow-y-scroll' id='page-content'>
 			<p class="opacity-87 text-xl lg:text-2xl xl:text-3xl font-normal">Stats</p>
+			<div class="w-1/2 chart mt-8 opacity-87">
+				<line-chart :options='options' :chart-data="datacollection"></line-chart>
+			</div>
 		</div>
 	</div>
 </template>
 
 <script>
 import Sidebar from '../components/Sidebar'
-export default {
-	name: 'Stats',
-	components: {
-		Sidebar
-	}
-}
+import LineChart from '../chart/linechart'
+
+ export default {
+    components: {
+			Sidebar,
+			LineChart
+    },
+    data () {
+      return {
+				datacollection: {},
+				datasets: [],
+				labels: ['Number of Cards'],
+				options: {
+						scales: {
+								yAxes: [{
+										ticks: {
+												beginAtZero: true
+										}
+								}]
+						},
+						defaultFontColor: '#ffffff'
+				},
+				chartBg: '#bb86fc',
+      }
+    },
+    methods: {
+			formatData() {
+				for (let i = 0; i < this.$store.state.decks.length; i++) {
+					this.datasets.push({
+						label: this.$store.state.decks[i].name,
+						data: [this.$store.state.decks[i].numOfWords],
+						backgroundColor: this.chartBg
+					})
+				}
+			},
+      fillData () {
+        this.datacollection = {
+					labels: this.labels,
+          datasets: this.datasets
+        }
+      }
+		},
+		created() {
+			this.$store.dispatch('getDecks')
+		},
+		mounted() {
+			this.formatData()
+      this.fillData()
+    }
+  }
 </script>
 
 <style scoped>
-
+.chart {
+	height: 50vh;
+}
 </style>
