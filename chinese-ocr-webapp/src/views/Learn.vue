f61a617e4e242948c16c12750752d4c751ae5ea8
TanayB11 <tanaybiradar24@gmail.com>
2020-06-03 11:47:21 -0700

    Scrollbar display on overflow

--- chinese-ocr-webapp/src/views/Learn.vue
+++ chinese-ocr-webapp/src/views/Learn.vue
@@ -3,7 +3,7 @@
 		<div class='col-span-1'>
 			<Sidebar/>
 		</div>
-		<div class='col-span-1 ml-8 md:ml-0 mt-10 md:mt-20 p-8 md:px-8 mt-12 md:mt-0' id='page-content'>
+		<div class='col-span-1 ml-8 md:ml-0 mt-10 md:mt-20 p-8 md:px-8 mt-12 md:mt-0 overflow-x-none overflow-y-auto' id='page-content'>
 			<p class="opacity-87 text-xl lg:text-2xl xl:text-3xl font-normal">Learn</p>
 			<DeckList view='learn'/>
 		</div>
