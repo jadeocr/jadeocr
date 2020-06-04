b73ea2eb7d5c9788a57c70a0baebbe7a69578fa1
Riley Kong <kongriley@gmail.com>
2020-06-04 16:16:58 -0700

    Add preliminary stats

--- chinese-ocr-webapp/src/views/Stats.vue
+++ chinese-ocr-webapp/src/views/Stats.vue
@@ -5,8 +5,13 @@
 		</div>
 		<div class='col-span-1 ml-8 md:ml-0 mt-10 md:mt-20 p-8 md:px-8 mt-12 md:mt-0 overflow-y-scroll' id='page-content'>
 			<p class="opacity-87 text-xl lg:text-2xl xl:text-3xl font-normal">Stats</p>
-			<div class="w-1/2 chart mt-8 opacity-87">
+			<div hidden class="w-1/2 chart mt-8 opacity-87">
 				<line-chart :options='options' :chart-data="datacollection"></line-chart>
+			</div>
+			<div class="mt-8 md:mt-10 opacity-87 text-md lg:text-lg xl:text-xl">
+				<h1 class="mb-4 text-lg lg:text-xl xl:text-2xl font-normal">General</h1>
+				<p>{{ $store.state.decks.length }} decks</p>
+				<p>{{ totalCards }} cards</p>
 			</div>
 		</div>
 	</div>
@@ -38,6 +43,7 @@ import LineChart from '../chart/linechart'
 						responsive: true
 				},
 				chartBg: '#bb86fc',
+				totalCards: 0,
       }
     },
     methods: {
@@ -59,6 +65,9 @@ import LineChart from '../chart/linechart'
 		},
 		created() {
 			this.$store.dispatch('getDecks')
+			for (let i = 0; i < this.$store.state.decks.length; i++) {
+				this.totalCards += this.$store.state.decks[i].numOfWords
+			}
 		},
 		mounted() {
 			this.formatData()
