48bcd9491f4f5083c1f8461013b488507d165941
TanayB11 <tanaybiradar24@gmail.com>
2020-06-04 10:18:13 -0700

    Add vision prediction on file

--- chinese-ocr-webapp/functions/index.js
+++ chinese-ocr-webapp/functions/index.js
@@ -2,35 +2,20 @@ const functions = require('firebase-functions')
 const vision = require('@google-cloud/vision')
 const client = new vision.ImageAnnotatorClient()
 
-exports.helloWorld = functions.https.onRequest((request, response) => {
-	response.set('Access-Control-Allow-Origin', '*')
-	console.log('Hello World!')
-	response.send("Hello from Firebase!")
-})
+// exports.helloWorld = functions.https.onRequest((request, response) => {
+// 	response.set('Access-Control-Allow-Origin', '*')
+// 	console.log('Hello World!')
+// 	response.send("Hello from Firebase!")
+// })
 
 exports.ocrVision = functions.https.onRequest(async (request, response) => {
-	response.set('Access-Control-Allow-Origin', '*')
+	response.set('Access-Control-Allow-Origin', '*') // TODO: Add whitelisted domains
 
-	const fileName = './vision-test-imgs/handwriting-master.jpg';
+	const fileName = './vision-test-imgs/handwriting-master.jpg'
 	// Read a local image as a text document
-	const [result] = await client.documentTextDetection(fileName);
-	const fullTextAnnotation = result.fullTextAnnotation;
-	console.log(`Full text: ${fullTextAnnotation.text}`);
-	fullTextAnnotation.pages.forEach(page => {
-		page.blocks.forEach(block => {
-			console.log(`Block confidence: ${block.confidence}`);
-			block.paragraphs.forEach(paragraph => {
-				console.log(`Paragraph confidence: ${paragraph.confidence}`);
-				paragraph.words.forEach(word => {
-					const wordText = word.symbols.map(s => s.text).join('');
-					console.log(`Word text: ${wordText}`);
-					console.log(`Word confidence: ${word.confidence}`);
-					word.symbols.forEach(symbol => {
-						console.log(`Symbol text: ${symbol.text}`);
-						console.log(`Symbol confidence: ${symbol.confidence}`);
-					})
-				})
-			})
+	client.documentTextDetection(fileName)
+		.then((result) => {
+			console.log(result)
 		})
-	})
+		.catch(error => console.log(error))
 })
