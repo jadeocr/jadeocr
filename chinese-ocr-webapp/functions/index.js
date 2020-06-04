68caca3191f38c0bd477bcf67de570fbd037c42a
TanayB11 <tanaybiradar24@gmail.com>
2020-06-04 08:49:25 -0700

    Remove functions

--- chinese-ocr-webapp/functions/index.js
+++
@@ -1,8 +0,0 @@
-const vision = require('@google-cloud/vision')({
-	projectId: 'your-project-id',
-	keyfileName: 'keyfile.json'
-})
-const admin = require('firebase-admin')
-const functions = require('firebase-functions')
-
-
